ef8142906784fa58b10eea195dae3ceb0c954f3b
Allexa <ahernandez12.build@gmail.com>
2021-10-25 12:42:15 -0700

    Project -1

--- week-1/my-calculator-app/src/main/java/com/revature/controller/ArithmeticController.java
+++ week-1/my-calculator-app/src/main/java/com/revature/controller/ArithmeticController.java
@@ -9,22 +9,53 @@ public class ArithmeticController {
 	
 	public ArithmeticService arithmeticService;
 	
-	public ArithmeticController() {
-		this.arithmeticService = new ArithmeticService();
+	public ArithmeticController(ArithmeticService arithmeticService) {
+		this.arithmeticService = arithmeticService;
 	}
 	
 	public Handler add = (ctx) -> {
 		//System.out.println("add lambda invoked");
-		ctx.result("add lambda invoked");
+		//ctx.result("add lambda invoked");
 		
-		String num1Str = ctx.formParam("number1");
-		String num2Str = ctx.formParam("number2");
+		String number1Str = ctx.formParam("number1");
+		String number2Str = ctx.formParam("number2");
 		
-		ctx.result(arithmeticService.doAddition(num1Str, num2Str));
+		ctx.result(arithmeticService.doAddition(number1Str, number2Str));
+	};
+	
+	public Handler subtract = (ctx) -> {
+		//ctx.result("subtract lambda invoked");
+		
+		String number3Str = ctx.formParam("number1");
+		String number4Str = ctx.formParam("number2");
+		
+		ctx.result(arithmeticService.doSubtraction(number3Str, number4Str));
+	};
+	
+	public Handler multiply = (ctx) -> {
+		//System.out.println("add lambda invoked");
+		//ctx.result("multiply lambda invoked");
+		
+		String number5Str = ctx.formParam("number1");
+		String number6Str = ctx.formParam("number2");
+		
+		ctx.result(arithmeticService.doMultiplication(number5Str, number6Str));
+	};
+	
+	public Handler divide = (ctx) -> {
+		//ctx.result("divide lambda invoked");
+		
+		String number7Str = ctx.formParam("number1");
+		String number8Str = ctx.formParam("number2");
+		
+		ctx.result(arithmeticService.doDivision(number7Str, number8Str));
 	};
 	
 	public void registerEndpoint(Javalin app) {
 		app.post("/add", add);
+		app.post("/subtract", subtract);
+		app.post("/multiply", multiply);
+		app.post("/divide", divide);
 	}
 	
 }
--- week-1/my-calculator-app/src/main/java/com/revature/service/ArithmeticService.java
+++ week-1/my-calculator-app/src/main/java/com/revature/service/ArithmeticService.java
@@ -1,14 +1,69 @@
 package com.revature.service;
 
 public class ArithmeticService {
-	public String doAddition(String num1Str, String num2Str) {
-		double num1 = Double.parseDouble(num1Str);
-		double num2 = Double.parseDouble(num2Str);
+	
+	/*
+	public String doCompare(String number1Str, String number2Str) {
+		double number1 = Double.parseDouble(number1Str);
+		double number2 = Double.parseDouble(number2Str);
 		
-		double sum = num1 + num2;
+		double sum = number1 + number2;
+		double difference = number1 - number2;
+		double product = number1 * number2;
+		double quotient = number1 / number2;
 		
-		String result = "" + sum;
+		String result1 = "Sum: " + sum;
+		String result2 = "Difference: " + difference;
+		String result3 = "Product: " + product;
+		String result4 = "Quotient: " + quotient;
+		
+		String result = result1 + "\n" + result2 + "\n" + result3 + "\n" + result4;
 		
 		return result;
+	} 
+	*/
+	
+	public String doAddition(String number1Str, String number2Str) {
+		double number1 = Double.parseDouble(number1Str);
+		double number2 = Double.parseDouble(number2Str);
+		
+		double sum = number1 + number2;
+		
+		String addResult = "Sum: " + sum;
+		
+		return addResult;
+	}
+	
+	public String doSubtraction(String number1Str, String number2Str) {
+		double number1 = Double.parseDouble(number1Str);
+		double number2 = Double.parseDouble(number2Str);
+		
+		double difference = number1 - number2;
+		
+		String subResult = "Difference: " + difference;
+		
+		return subResult;
+	}
+	
+	public String doMultiplication(String number1Str, String number2Str) {
+		double number1 = Double.parseDouble(number1Str);
+		double number2 = Double.parseDouble(number2Str);
+		
+		double product = number1 * number2;
+		
+		String mulResult = "Product: " + product;
+		
+		return mulResult;
+	}
+	
+	public String doDivision(String number1Str, String number2Str) {
+		double number1 = Double.parseDouble(number1Str);
+		double number2 = Double.parseDouble(number2Str);
+		
+		double quotient = number1 / number2;
+		
+		String divResult = "Quotient: " + quotient;
+		
+		return divResult;
 	}
 }
